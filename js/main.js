0ee0511b176b1bfb54b718bccdb229f447cfba43
GabrielAtenciaGomez <gabriel.atencia@cecar.edu.co>
2020-01-13 17:36:48 -0600

    Se agregaron los clientes al carousel y se alinearon para una mejor presentacion

--- js/main.js
+++ js/main.js
@@ -160,12 +160,14 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
+
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
     dots: true,
     loop: true,
-    items: 1
+    items: 1,
+    
   });
 
   $("#btnfacebook").click(function(){
